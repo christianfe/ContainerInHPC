d1ce233a78d42776a95250e188802c01bf793445
Christian Felicione <christian.felicione@gmail.com>
2023-11-24 22:49:03 +0100

    vuln web app

---
+++ Image scanner/examples/vulnerable_webapp/index.php
@@ -0,0 +1,41 @@
+<?php
+$target = $_POST["ip"];
+$cmd = isset($_POST["submit"]) && $target != "" ? shell_exec("ping -c 3" . $target) : "";
+?>
+<html>
+
+<head>
+	<title>My Ping</title>
+	<style>
+		body {
+			background-color: #617867;
+		}
+
+		.container {
+			width: 1000px;
+			margin: auto;
+			margin-top: 10px;
+			word-wrap: break-word;
+			background-color: #b4d9be;
+			padding: 1em;
+		}
+
+		form,
+		h1 {
+			text-align: center;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="container">
+		<h1>Enter the IP to ping</h1>
+		<form method="POST">
+			<input type="text" name="ip" value="<?php echo $target ?>">
+			<input type="submit" name="submit" value="Go!">
+		</form>
+		<pre><?php echo  $cmd ?></pre>
+	</div>
+</body>
+
+</html>
